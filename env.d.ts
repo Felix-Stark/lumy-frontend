b2ff3a6f7c6558439ee22fb84a7fa5514e9a4790
Felix Stark <felix.b.stark@gmail.com>
2025-05-18 00:02:28 +0200

    declared .vue

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+declare module "*.vue" {
+  import { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
+declare module "*.svg" {
+  const content: any;
+  export default content;
+}

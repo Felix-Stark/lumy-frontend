8c68d132c3c5390bc3948d1a78ddc9a12cd94f9d
Felix Stark <felix.b.stark@gmail.com>
2025-05-19 13:27:47 +0200

    authed routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,7 +11,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: () => import('@/views/HomeView.vue'),
+      component: () => import('@/views/Home.vue'),
       meta: {
         title: 'Home',
         requiresAuth: true,

e4b7132f717153d8d4235810bda5991e86454bc3
Felix Stark <felix.b.stark@gmail.com>
2025-05-22 12:51:39 +0200

    removed headcard

--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -5,11 +5,11 @@
 <template>
   <div class="flex flex-col h-screen w-full mx-0-auto bg-[#EFEFF9]">
     <header class="">
-      
+
     </header>
   </div>
 </template>
 
 <script setup lang="ts">
-  import HeadCard from '@/components/HeadCard.vue';
+
 </script>

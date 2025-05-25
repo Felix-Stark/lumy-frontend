8e084275f85bf82572a8aef4cc0ce5f4d31e1cd6
oscarklink <oscarklink@gmail.com>
2025-05-25 20:29:08 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,12 +6,13 @@ export default {
   safelist: [
     'bg-mypurple',
     'hover:bg-mypurple-light',
+    'text-mypurple',
   ],
   theme: {
     extend: {
       colors: {
         mypurple: {
-          DEFAULT: "#962DFF",
+          DEFAULT: "#962DFF",   // add a comment to trigger rebuild
           light: "#962dffb5",
         },
       },

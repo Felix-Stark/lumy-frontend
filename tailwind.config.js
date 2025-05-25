40f90158201dee1110d7455f69e3209760cc1eb9
oscarklink <oscarklink@gmail.com>
2025-05-25 21:15:38 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  safelist: ['bg-mypurple', 'hover:bg-mypurple-light'],
+  theme: {
+    extend: {
+      colors: {
+        mypurple: {
+          DEFAULT: '#962DFF',
+          light: '#962dffb5',
+        },
+      },
+    },
+  },
+  plugins: [],
+};

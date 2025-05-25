ecee44b9efb1b777552533f7cb4b2554822a6f05
oscarklink <oscarklink@gmail.com>
2025-05-25 21:09:39 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,27 @@
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  safelist: [
-    'bg-mypurple',
-    'hover:bg-mypurple-light',
-    'text-mypurple',
-  ],
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         mypurple: {
-          DEFAULT: "#962DFF",   // add a comment to trigger rebuild
+          DEFAULT: "#962DFF",
           light: "#962dffb5",
         },
+        mygray: {
+          DEFAULT: "#f3f4f6",
+        },
+        // Add more custom colors here
       },
+      fontFamily: {
+        inter: ["Inter Variable", "sans-serif"],
+        // Add more custom fonts here
+      },
+      // Add more customizations (spacing, borderRadius, etc.) here
     },
   },
   plugins: [],
 };
+
+export default config

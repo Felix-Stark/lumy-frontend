0e76cea957d9fa7a0cffeb4f0d0ff1d8b2acd807
oscarklink <oscarklink@gmail.com>
2025-05-25 20:07:40 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,8 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+const config = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
@@ -9,20 +10,10 @@ const config: Config = {
           DEFAULT: "#962DFF",
           light: "#962dffb5",
         },
-
-        mygray: {
-          DEFAULT: "#f3f4f6",
-        },
-        // Add more custom colors here
       },
-      fontFamily: {
-        inter: ["Inter Variable", "sans-serif"],
-        // Add more custom fonts here
-      },
-      // Add more customizations (spacing, borderRadius, etc.) here
     },
   },
   plugins: [],
 };
 
-export default config
+export default config;

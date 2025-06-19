dee1ed59b1cdc1819a0ec4452bab7f1e1c1ef3de
Felix Stark <felix.b.stark@gmail.com>
2025-06-19 09:11:51 +0200

    added disabled to props

--- src/components/base/BaseButton.vue
+++ src/components/base/BaseButton.vue
@@ -1,5 +1,6 @@
 <template>
 	<button
+  :disabled="disabled"
     class="bg-lumy-purple hover:opacity-50 max-w-fit text-white font-bold py-2 px-4 rounded-md flex items-center justify-center mt-4 cursor-pointer transition duration-200 ease-in-out"
     @click="onAction"
   >
@@ -11,6 +12,8 @@
 <script setup lang="ts">
 import { defineProps } from 'vue';
 defineProps<{
+  disabled?: boolean;
+  loading?: boolean;
   btnText: string;
   onAction: () => void;
 }>();

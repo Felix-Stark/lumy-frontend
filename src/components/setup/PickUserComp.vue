80eed5e4709b186d23edcb6dbec4b01b145116ca
Felix Stark <felix.b.stark@gmail.com>
2025-07-03 00:48:40 +0200

    switched to text button on user

--- src/components/setup/PickUserComp.vue
+++ src/components/setup/PickUserComp.vue
@@ -1,8 +1,5 @@
 <template>
-	<div :key="id" :class="[
-            'flex w-full not-last:border-b-2 border-gray-200',
-            
-        ]">
+	<div :key="id" class="flex w-full not-last:border-b-2 border-gray-200">
 		<div :class="['flex items-center justify-start w-full gap-4 p-2', isActive === true ? '' : 'opacity-50']">
 			<!-- Avatar and Name Section -->
 			<img
@@ -40,9 +37,11 @@
 					</ListboxOptions>
 				</div>
 			</Listbox>
-			<button @click="emit('update:isActive', !isActive)">
-				<UserMinus v-if="isActive === true" class="w-6 h-6 cursor-pointer text-red-500" />
-				<UserPlus v-else class="w-6 h-6 text-green-500  cursor-pointer" />
+			<button
+			:class="['px-4 py-2 text-white', isActive === true ? 'bg-red-500' : 'bg-green-500']" 
+			@click="emit('update:isActive', !isActive)"
+			>
+				{{ isActive === true ? 'Remove' : 'Add' }}
 			</button>
 		</div>
 	</div>

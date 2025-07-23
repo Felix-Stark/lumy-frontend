fc56806f43a1d501168dab038cb67ae3386a9284
Felix Stark <felix.b.stark@gmail.com>
2025-07-23 22:47:04 +0200

    added gap to member dashobard

--- src/layouts/MemberLayout.vue
+++ src/layouts/MemberLayout.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="flex flex-col items-center gap-6 w-full p-4 md:w-[80vw] h-full">
 		<NavUtility />
-        <main class="flex flex-col items-center w-full mt-12">
+        <main class="flex flex-col items-center gap-6 w-full mt-12">
         <router-view />
         </main>
     </div>

4967c904869d4ab33de0c66e59ad420b54e8dac4
oscarklink <oscarklink@gmail.com>
2025-08-01 21:28:59 +0200

    Fix typo in redirect URI

--- src/views/auth/slack/SlackRegister.vue
+++ src/views/auth/slack/SlackRegister.vue
@@ -13,7 +13,7 @@
 import SlackFlowComp from '@/components/slackFlow/SlackFlowComp.vue'
 import lumyGlad from '@/assets/images/lumy_glad.png'
 const clientId = import.meta.env.VITE_SLACK_CLIENT_ID
-const redirectUri = 'https://app.getlumy.app//slack/install-redirect'
+const redirectUri = 'https://app.getlumy.app/slack/install-redirect'
 const slackOauthUrl = `https://slack.com/oauth/v2/authorize?client_id=${clientId}&scope=commands,chat:write,users:read,team:read,users:read.email&redirect_uri=${redirectUri}`
 
 

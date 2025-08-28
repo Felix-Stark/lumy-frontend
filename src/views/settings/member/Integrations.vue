dcc1e23fa6d9e8bf8ca1b914881ba8e88c8a5fb2
Felix Stark <felix.b.stark@gmail.com>
2025-08-28 13:23:43 +0200

    Update Integrations.vue

--- src/views/settings/member/Integrations.vue
+++ src/views/settings/member/Integrations.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col items-center gap-6 w-full min-h-[60vh] p-8 bg-white rounded-xl shadow-md">
+    <div class="flex flex-col items-center gap-6 w-full min-h-[60vh] p-8 lg:p-12 bg-white rounded-xl shadow-md">
         <img :src="GoogleCal" alt="Google calendar logo" class="w-48 h-48 mb-4"/>
         <h1 class="font-inter text-2xl">Google Calendar</h1>
         <div class="flex flex-col justify-center items-center mt-4 font-normal text-sm space-y-1">
@@ -25,10 +25,10 @@
             <p class="text-lumy-danger">Not connected</p>
         </div>
         <div class="flex space-x-4 mt-6">
-            <button v-if="isConnected === true" class="flex items-center space-x-2 px-4 py-2 bg-lumy-dark opacity-70 text-white text-sm rounded-lg hover:bg-gray-300 transition">
+            <button v-if="isConnected === true" class="flex items-center space-x-2 px-4 py-2 bg-lumy-dark opacity-70 text-white text-sm rounded-lg">
                 Disconnect
             </button>
-            <button v-else @click="triggerGoogle" class="flex items-center space-x-2 px-4 py-2 bg-lumy-green text-white text-sm rounded-lg hover:bg-lumy-darker transition">
+            <button v-else @click="triggerGoogle" class="flex items-center space-x-2 px-4 py-2 bg-lumy-green text-white text-sm rounded-lg">
                 Connect
             </button>
         </div>

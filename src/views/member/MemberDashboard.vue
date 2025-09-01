d100c92256dd43d89e9f5699f4bfb5f50fbaaf98
Felix Stark <felix.b.stark@gmail.com>
2025-09-01 14:40:48 +0200

    Update MemberDashboard.vue

--- src/views/member/MemberDashboard.vue
+++ src/views/member/MemberDashboard.vue
@@ -69,7 +69,7 @@
 							<td v-if="skill.average_sentiment === 0" class="px-4 py-2 text-gray-500">No feedback</td>
 							<td v-if="skill.average_sentiment <= 0.40 && skill.average_sentiment > 0" class="px-4 py-2 text-red-500">Needs improvement</td>
 							<td class="px-4 py-2 text-center">{{ skill.feedback_count }}</td>
-							<td class="px-4 py-2">{{ skill.last_feedback_received ?  formatFeedbackDate(lastFeedback) : 'None' }}</td>
+							<td class="px-4 py-2">{{ skill.last_feedback_received ?  formatFeedbackDate(skill.last_feedback_received) : 'None' }}</td>
 							<td class="px-4 py-2 flex justify-between items-center">
 								<button @click="selectedSkill(skill)" class=" bg-lumy-purple text-white font-bold py-2 px-4 rounded-md cursor-pointer">
 									Request

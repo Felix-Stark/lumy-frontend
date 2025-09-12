87af2eb0167ad3fe7e5bfb3f1108a2a70590b036
Felix Stark <felix.b.stark@gmail.com>
2025-09-12 08:51:06 +0200

    Update DashboardLayout.vue

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="flex flex-col items-center gap-6 w-full p-4 md:w-4/5 lg:w-4/6 h-full">
 		<NavUtility />
-        <main class="flex flex-col items-center gap-6 w-full mt-12">
+        <main class="flex flex-col items-center gap-6 w-full my-12">
         <router-view />
         </main>
     </div>

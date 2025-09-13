ea2ac5cccbe06da32aaa6b15f410eec052d59ebb
Felix Stark <felix.b.stark@gmail.com>
2025-09-13 12:41:03 +0200

    Update Overview.vue

--- src/views/feedback/Overview.vue
+++ src/views/feedback/Overview.vue
@@ -39,7 +39,7 @@ const feedbackList = ref<FeedbackSubmission[]>([]);
 const positiveSentiments = computed(() => {
     const positive = feedbackList.value.filter(fb => fb.sentiment === 'positive');
     console.log('positive array: ', positive);
-    return (positive.length / feedbackList.value.length);
+    return (positive.length / feedbackList.value.length) * 100;
 })
 
 onMounted(async() => {

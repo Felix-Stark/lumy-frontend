e6b8ce56580e95eee27ee7572bcbd1db0daccf68
Felix Stark <felix.b.stark@gmail.com>
2025-09-15 19:59:06 +0200

    Update Overview.vue

--- src/views/feedback/Overview.vue
+++ src/views/feedback/Overview.vue
@@ -102,9 +102,9 @@
         </div>
 
     </section>
-    <section class="flex flex-col lg:flex-row lg:flex-wrap justify-between w-full mt-6">
+    <section class="flex flex-col lg:flex-row lg:flex-wrap justify-between w-full mt-6 space-y-8">
         <div v-if="feedbackList.length === 0" class="text-gray-500">No feedback available.</div>
-        <div v-else v-for="feedback in feedbackList" :key="feedback.id" class="flex flex-col bg-white shadow-md rounded-lg p-8 w-full gap-8 lg:max-w-7/16 ">
+        <div v-else v-for="feedback in feedbackList" :key="feedback.id" class="flex flex-col bg-white shadow-md rounded-lg p-8 w-full gap-8 lg:max-w-1/2 ">
             <p class="font-light text-sm text-gray-600">{{ feedback.feedback_request?.recipient.name }}</p>
             <p class="text-gray-800">{{ feedback.content }}</p>
             <div class="mt-2 text-sm text-gray-500 flex justify-between">

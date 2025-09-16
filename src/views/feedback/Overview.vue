9990f474005ba592117573bd8ebf481bca6741d3
Felix Stark <felix.b.stark@gmail.com>
2025-09-16 22:39:42 +0200

    Update Overview.vue

--- src/views/feedback/Overview.vue
+++ src/views/feedback/Overview.vue
@@ -47,7 +47,7 @@
                     </span>
                     <ChevronDown class="ml-2 size-4"/>
                 </ListboxButton>
-                    <ListboxOptions class="bg-white aboslute max-h-60 w-full shadow-lg p-2 overflow-y-auto">
+                    <ListboxOptions class="bg-white aboslute max-h-60 min-w-full shadow-lg p-2 overflow-y-auto">
                         <ListboxOption
                         v-for="s in summary?.skills_summary"
                         v-slot="{ active, selected }"
@@ -68,13 +68,13 @@
                 :flip="true"
                 :offset="2"
                 >
-                <ListboxButton class="flex items-center px-4 py-2 rounded-lg hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-lumy-purple cursor-pointer">
+                <ListboxButton class="flex items-center px-4 py-2 rounded-lg hover:bg-gray-50 focus:outline-none cursor-pointer">
                     <span class="block">
                         Filter by Peer
                     </span>
                     <ChevronDown class="ml-2 size-4"/>
                 </ListboxButton>
-                    <ListboxOptions class="bg-white max-h-60 w-full p-2 shadow-lg overflow-y-auto">
+                    <ListboxOptions class="bg-white max-h-60 min-w-full p-2 shadow-lg overflow-y-auto">
                         <ListboxOption
                         v-for="submitter in submitters"
                         v-slot="{ active, selected }"
@@ -95,13 +95,13 @@
                 :flip="true"
                 :offset="2"
                 >
-                    <ListboxButton class="flex items-center px-4 py-2 rounded-lg hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-lumy-purple cursor-pointer">
+                    <ListboxButton class="flex items-center px-4 py-2 rounded-lg hover:bg-gray-50 focus:outline-none cursor-pointer">
                         <span class="block">
                             Filter by Sentiment
                         </span>
                         <ChevronDown class="ml-2 size-4"/>
                     </ListboxButton>
-                    <ListboxOptions class="bg-white max-h-60 p-2 overflow-y-auto w-full">
+                    <ListboxOptions class="bg-whit min-w-full max-h-60 p-2 overflow-y-auto w-full">
                         <ListboxOption
                         v-for="sentiment in ['positive', 'neutral', 'negative']"
                         v-slot="{ active, selected }"

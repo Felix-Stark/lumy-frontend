8d4f3dd9318807b935923e3aa51f6f3734bec161
Felix Stark <felix.b.stark@gmail.com>
2025-09-19 20:53:16 +0200

    debug

--- src/views/auth/slack/SlackLogin.vue
+++ src/views/auth/slack/SlackLogin.vue
@@ -14,6 +14,7 @@
 
 
 <script setup lang="ts">
+console.log('SLACK LOGIN VIEW');
 import { useRouter } from 'vue-router';
 import lumyLogo from '@/assets/images/lumy.png';
 import SlackFlowComp from '@/components/slackFlow/SlackFlowComp.vue';

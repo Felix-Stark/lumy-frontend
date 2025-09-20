8bb0001ad86a20315fd871b44426dc41f12281d0
Felix Stark <felix.b.stark@gmail.com>
2025-09-20 11:58:22 +0200

    sorted types, skill avg sent

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,47 @@
+export type TimeSeries = Record<string, number>;
+
+// ONBOARDING
+export type SetupAccount = {
+	  id: number;
+	  account_id: number;
+	  role: string;
+	  users: SetupUser[]; // Array of users in the account
+}
+
+export type SetupUser = {
+  id: number;
+  name: string;
+  email: string;
+  role: string;
+  is_active: boolean;
+  avatar: string;
+  title?: string;
+};
+
+// ACCOUNT
+export type Account = {
+  id: number;
+  name: string;
+  framework_id: number;
+  bot_personality_id: number;
+  nudge_interval_weeks: number;
+  nudge_weekday: number; // 1 (Monday) to 5 (Friday)
+  nudge_hour: number; // 0 to 23
+  intelligence_coach: boolean;
+  intelligence_assistant: boolean;
+  created_at: string;
+  updated_at: string;
+};
+
+export type BotPersonality = {
+  id: number;
+  name: string;
+  description: string;
+  formatted_name: string;
+}
+
+// USER TYPES
+
 export type User = {
   id: number;
   name: string;
@@ -10,8 +54,6 @@ export type User = {
   account_id: string; // ID of the account this user belongs to
   is_active: boolean;
 };
-
-export type TimeSeries = Record<string, number>;
 
 export type UserSummary = {
   feedback_received_count: number;
@@ -30,6 +72,15 @@ export type UserSummary = {
   avg_sentiment: TimeSeries;
 };
 
+// SKILL TYPES
+
+export type Skill = {
+  id: number;
+  skill: string;
+  definition: string;
+  theme: string;
+}
+
 export type SkillSummary = {
   skill_id: number;
   name: string;
@@ -38,43 +89,33 @@ export type SkillSummary = {
   last_feedback_received: string; // ISO date string
 }
 
-export type Account = {
-  id: number;
-  name: string;
-  framework_id: number;
-  bot_personality_id: number;
-  nudge_interval_weeks: number;
-  nudge_weekday: number; // 1 (Monday) to 5 (Friday)
-  nudge_hour: number; // 0 to 23
-  intelligence_coach: boolean;
-  intelligence_assistant: boolean;
-  created_at: string;
-  updated_at: string;
-};
-
-export type SetupAccount = {
-	  id: number;
-	  account_id: number;
-	  role: string;
-	  users: SetupUser[]; // Array of users in the account
+export type SkillOverview = {
+  skill_name: string;
+  skill_definition: string;
+  average_sentiment_over_time: {
+    avg_sentiment: number;
+    month: string; // ISO date string
+  }[];
+  feedback_received: {
+    id: number;
+    content: string;
+    sentiment: string; // e.g. 'positive' | 'neutral' | 'negative'
+    sentiment_score: number; // e.g. 85
+    created_at: string; // ISO date string
+    feedback_request_id: string;
+    constructiveness_score: number; // e.g. 70
+  } [];
+  submission_counts: {
+    by_sentiment: {
+      positive?: number;
+      neutral?: number;
+      negative?: number;
+    };
+    total: number;
+  }
 }
 
-export type SetupUser = {
-  id: number;
-  name: string;
-  email: string;
-  role: string;
-  is_active: boolean;
-  avatar: string;
-  title?: string;
-};
-
-export type Skill = {
-  id: number;
-  skill: string;
-  definition: string;
-  theme: string;
-}
+// FEEDBACK TYPES
 
 export type FeedbackRequest = {
   id: number;
@@ -126,13 +167,6 @@ export type FeedbackSubmission = {
   updated_at: string; // ISO date string
   feedback_request?: FeedbackRequestShort;
 };
-
-export type BotPersonality = {
-  id: number;
-  name: string;
-  description: string;
-  formatted_name: string;
-}
 
 export type FeedbackFramework = {
   id: number;
--- src/views/member/SkillOverview.vue
+++ src/views/member/SkillOverview.vue
@@ -13,8 +13,8 @@
 
 <script setup lang="ts">
 import BaseButton from '@/components/base/BaseButton.vue';
-import { ref, computed, onMounted } from 'vue';
-import type { Skill, SkillSummary } from '@/types';
+import { ref, computed, onMounted, onUnmounted } from 'vue';
+import type { Skill, SkillSummary, SkillOverview } from '@/types';
 import { useRouter } from 'vue-router';
 import { formatName } from '@/composables/formatName';
 import api from '@/services/api';
@@ -24,6 +24,7 @@ const router = useRouter();
 const activeSkill = computed<SkillSummary>(() => {
     return JSON.parse(sessionStorage.getItem('selectedSkill') || '{}');
 });
+const skillOv = ref<SkillOverview | null>(null);
 
 onMounted(async() => {
     const res = await api.get(`/me/skill/${activeSkill.value.skill_id}`);
@@ -31,4 +32,44 @@ onMounted(async() => {
         console.log('Skill data: ', res.data);
     }
 });
+onUnmounted(() => {
+    sessionStorage.removeItem('selectedSkill');
+});
+
+const avgSentChart = computed(() => {
+	const labels = skillOv.value?.average_sentiment_over_time.map((item) =>
+        new Date(item.month).toLocaleString("default", { month: "short", year: "numeric" })
+    );
+    const data = skillOv.value?.average_sentiment_over_time.map((item) => item.avg_sentiment);
+	return {
+		labels, // e.g. ["2025-07", "2025-08", ...]
+		datasets: [
+			{
+				label: 'Average Sentiment',
+				data,
+				fill: false,
+				borderColor: 'rgba(150, 45, 255, 1)',
+				borderDash: [ 5, 5 ],
+				tension: 0.4
+			}
+		]
+	};
+})
+
+const avgSentOptions = {
+  responsive: true,
+  maintainAspectRatio: true,
+  plugins: {
+    legend: { display: false },
+    title: { display: false }
+  },
+  scales: {
+    y: {
+      min: 0,
+      max: 100+'%',
+      ticks: { stepSize: 20 }
+    }
+  }
+};
+
 </script>

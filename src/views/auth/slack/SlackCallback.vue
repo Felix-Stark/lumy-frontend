9ca46ef1ad6e9f348c32738eb965a8e8da9c6924
Felix Stark <felix.b.stark@gmail.com>
2025-09-21 11:42:03 +0200

    Update SlackCallback.vue

--- src/views/auth/slack/SlackCallback.vue
+++ src/views/auth/slack/SlackCallback.vue
@@ -27,13 +27,14 @@ onMounted( async () => {
 		const path = await authStore.loginSlack(code as string);
 		if (path !== undefined) {
 			router.push(path);
-		} else {
-			console.error('Login failed');
+		}
+		if(route.query.error === 'access_denied') {
+			router.push({ name: 'slack-login' });
 		}
 	}
 	} catch (error) {
 		console.error('Error during Slack callback: ', error);
-		router.push({ name: 'SlackError' });
+		router.push({ name: 'error' });
 	}
 });
 

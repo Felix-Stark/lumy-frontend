7197ce2adf6dcd28998e6e25c0669abe9b5c1e92
Felix Stark <felix.b.stark@gmail.com>
2025-09-26 21:05:10 +0200

    Update FeedbackOverview.vue

--- src/views/member/FeedbackOverview.vue
+++ src/views/member/FeedbackOverview.vue
@@ -161,7 +161,7 @@
         </div>
     </section>
     <div v-if="filteredSentiment || filteredSkill || filteredSubmitter " class="flex gap-2 w-full">
-        <p class="text-sm">Filter by: </p><span class="ml-2 bg-purple-100 text-purple-800 text-xs px-3 py-1 rounded-full" v-if="filteredSkill">Skill: {{ filteredSkill }}</span><span v-if="filteredSubmitter" class="bg-purple-100 text-purple-800 text-xs px-3 py-1 rounded-full">Peer: {{ formatName(feedbackList.find((f) => f?.feedback_request?.recipient_id === filteredSubmitter)?.feedback_request?.recipient.name ?? '') }}</span><span v-if="filteredSentiment" class="bg-purple-100 text-purple-800 text-xs px-3 py-1 rounded-full">Sentiment: {{ formatName(filteredSentiment) }}</span><span v-if="filteredStatus" class="bg-purple-100 text-purple-800 text-xs px-3 py-1 rounded-full">Status: {{ formatName(filteredStatus) }}</span>
+        <p class="text-sm">Filter by: </p><span class="ml-2 bg-purple-100 text-purple-800 text-xs px-3 py-1 rounded-full" v-if="filteredSkill">Skill: {{ filteredSkill }}</span><span v-if="filteredSubmitter" class="bg-purple-100 text-purple-800 text-xs px-3 py-1 rounded-full">Peer: {{ formatName(feedbackList.find((f) => currentFilter === 'given' ? f?.feedback_request?.sender_id : f?.feedback_request?.recipient_id === filteredSubmitter)?.feedback_request?.recipient.name ?? '') }}</span><span v-if="filteredSentiment" class="bg-purple-100 text-purple-800 text-xs px-3 py-1 rounded-full">Sentiment: {{ formatName(filteredSentiment) }}</span><span v-if="filteredStatus" class="bg-purple-100 text-purple-800 text-xs px-3 py-1 rounded-full">Status: {{ formatName(filteredStatus) }}</span>
     </div>
     <section class="flex flex-col lg:flex-row lg:flex-wrap justify-between w-full gap-8 space-y-8">
         <div v-if="feedbackList.length === 0" class="text-gray-500">No feedback available.</div>
@@ -308,7 +308,7 @@ const filter = computed(() => {
 
     const matchesSubmitter =
       !filteredSubmitter.value ||
-      fb?.feedback_request?.recipient.id === filteredSubmitter.value;
+      currentFilter.value === 'given' ? fb?.feedback_request?.sender.id : fb?.feedback_request?.recipient.id === filteredSubmitter.value;
 
     const matchesSentiment =
       !filteredSentiment.value || fb?.sentiment === filteredSentiment.value;

f8a02495d2acef54c0de6e80f8edf9d88f4b7117
Felix Stark <felix.b.stark@gmail.com>
2025-09-29 22:35:56 +0200

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -86,7 +86,7 @@ const router = createRouter({
       component: DashboardLayout,
       redirect: '/overview/admin',
       meta: {
-        requiresRole: true,
+        isAdmin: true,
         requiresAuth: true,
       },
       children: [

7762a8406db15dda1c84691beb4c384e5ba6407f
Felix Stark <felix.b.stark@gmail.com>
2025-10-13 22:48:40 +0200

    Update SettingsGeneral.vue

--- src/components/skeletons/SettingsGeneral.vue
+++ src/components/skeletons/SettingsGeneral.vue
@@ -1,24 +1,24 @@
 <template>
     <div class="animate-pulse w-full p-8 flex flex-col bg-white rounded-xl shadow-md">
         <div class="flex flex-col gap-8 md:w-1/2">
-            <div class="h-6 bg-gray-500 rounded w-50"></div>
+            <div class="h-6 bg-gray-200 rounded w-50"></div>
             <div class="flex flex-col w-full pl-8 pb-6 border-l border-gray-300">
-                <div class="h-4 bg-gray-400 rounded w-1/4 mb-2"></div>
-                <div class="h-4 bg-gray-400 rounded w-full"></div>
+                <div class="h-4 bg-gray-200 rounded w-1/4 mb-2"></div>
+                <div class="h-4 bg-gray-200 rounded w-full"></div>
             </div>
             <div class="flex flex-col w-full pl-8 pb-6 border-l border-gray-300">
-                <div class="h-4 bg-gray-400 rounded w-1/4 mb-2"></div>
-                <div class="h-4 bg-gray-400 rounded w-full"></div>
+                <div class="h-4 bg-gray-200 rounded w-1/4 mb-2"></div>
+                <div class="h-4 bg-gray-200 rounded w-full"></div>
             </div>
             <div class="flex flex-col w-full pl-8 pb-6 border-l border-gray-300">
-                <div class="h-4 bg-gray-400 rounded w-1/4 mb-2"></div>
-                <div class="h-4 bg-gray-400 rounded w-full"></div>
+                <div class="h-4 bg-gray-200 rounded w-1/4 mb-2"></div>
+                <div class="h-4 bg-gray-200 rounded w-full"></div>
             </div>
             <hr class="text-gray-300" />
-            <div class="h-6 bg-gray-500 rounded w-50"></div>
+            <div class="h-6 bg-gray-200 rounded w-50"></div>
             <div class="flex flex-col w-full pl-8 pb-6 border-l border-gray-300">
-                <div class="h-4 bg-gray-400 rounded w-1/4 mb-2"></div>
-                <div class="h-4 bg-gray-400 rounded w-full"></div>
+                <div class="h-4 bg-gray-200 rounded w-1/4 mb-2"></div>
+                <div class="h-4 bg-gray-200 rounded w-full"></div>
             </div>
         </div>
     </div>

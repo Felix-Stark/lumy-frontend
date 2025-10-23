1e0cdc9470da65a3a4f38f4dddc179c3cdbaa6aa
Felix Stark <felix.b.stark@gmail.com>
2025-10-23 08:58:37 +0200

    Update FeedbackOverview.vue

--- src/views/member/FeedbackOverview.vue
+++ src/views/member/FeedbackOverview.vue
@@ -1,6 +1,5 @@
 <template>
     <FeedbackSkeleton v-if="initLoading" />
-    <div v-else class="flex flex-col items-center gap-6 w-full lg:my-20">
         <section class="flex flex-col md:flex-row w-full gap-6 lg:gap-8">
             <div class="bg-white shadow-lg rounded-lg p-8 w-full flex justify-center items-center">
                 <div class="relative h-48">
@@ -165,6 +164,7 @@
         <div v-if="filteredSentiment || filteredSkill || filteredSubmitter " class="flex gap-2 w-full">
             <p class="text-sm">Filter by: </p><span class="ml-2 bg-purple-100 text-purple-800 text-xs px-3 py-1 rounded-full" v-if="filteredSkill">Skill: {{ filteredSkill }}</span><span v-if="filteredSubmitter" class="bg-purple-100 text-purple-800 text-xs px-3 py-1 rounded-full">Peer: {{ filterPeersList }}</span><span v-if="filteredSentiment" class="bg-purple-100 text-purple-800 text-xs px-3 py-1 rounded-full">Sentiment: {{ formatName(filteredSentiment) }}</span><span v-if="filteredStatus" class="bg-purple-100 text-purple-800 text-xs px-3 py-1 rounded-full">Status: {{ formatName(filteredStatus) }}</span>
         </div>
+        <div v-else class="flex flex-col items-center gap-6 w-full lg:my-20">
         <section class="flex flex-col lg:flex-row lg:flex-wrap justify-between w-full gap-8 space-y-8">
             <div v-if="currentFilter === 'received' && filter.length === 0" class="text-gray-500">
                 No feedback available.

9b2013309e9c9c20895bbf60ed7b1b068ee2f3a4
Felix Stark <felix.b.stark@gmail.com>
2025-11-26 18:49:38 +0100

    Update Skills.vue

--- src/views/settings/admin/Skills.vue
+++ src/views/settings/admin/Skills.vue
@@ -14,46 +14,47 @@
         <section class="flex flex-col gap-6">
             <Disclosure v-slot="{ open }" v-for="s in skills">
                 <div class="flex w-full justify-between border-b border-b-gray-300">
-                    <div class="flex flex-col"></div>
-                    <DisclosureButton class="flex w-full cursor-pointer justify-between p-2 hover:text-lumy-purple/40 ">
-                        <span class="font-bold text-gray-600">{{ s.skill }}</span>
-                        <ChevronDown class="w-5 h-5 text-gray-600" :class="open && 'rotate-180'" />
-                    </DisclosureButton>
-                    <div class="flex gap-6">
-                        <Switch
-                            v-model="s.is_active"
-                            class="relative inline-flex h-6 w-11 items-center rounded-full transition-colors focus:outline-none cursor-pointer"
-                            :class="active ? 'bg-lumy-green' : 'bg-lumy-danger-light'"
-                        >
-                            <span class="sr-only">Enable skill</span>
-                            <span
-                            class="inline-block h-4 w-4 transform rounded-full bg-white transition-transform"
-                            :class="active ? 'translate-x-6' : 'translate-x-1'"
-                            />
-                        </Switch>
-                        <Menu as="div" class="relative inline-block text-left">
-                            <MenuButton>
-                                <EllipsisVertical class="h-6" />
-                            </MenuButton>
-                            <MenuItems class="w-36 bg-white aboslute bottom-0">
-                                <MenuItem class="flex items-center gap-4">
-                                    <button @click="() => editSkill(s)" class="cursor-pointer">
-                                        <Edit class="w-5 h-5 text-lumy-secondary" aria-hidden="true" />
-                                    </button>
-                                    Edit
-                                </MenuItem>
-                                <MenuItem class="flex items-center gap-4">
-                                    <button @click="() => editSkill(s)" class="cursor-pointer">
-                                        <Delete class="w-5 h-5 text-lumy-secondary" aria-hidden="true" />
-                                    </button>
-                                    Delete
-                                </MenuItem>
-                            </MenuItems>
-                        </Menu>
+                    <div class="flex flex-col">
+                        <DisclosureButton class="flex w-full cursor-pointer justify-between p-2 hover:text-lumy-purple/40 ">
+                            <span class="font-bold text-gray-600">{{ s.skill }}</span>
+                            <ChevronDown class="w-5 h-5 text-gray-600" :class="open && 'rotate-180'" />
+                        </DisclosureButton>
+                        <div class="flex gap-6">
+                            <Switch
+                                v-model="s.is_active"
+                                class="relative inline-flex h-6 w-11 items-center rounded-full transition-colors focus:outline-none cursor-pointer"
+                                :class="active ? 'bg-lumy-green' : 'bg-lumy-danger-light'"
+                            >
+                                <span class="sr-only">Enable skill</span>
+                                <span
+                                class="inline-block h-4 w-4 transform rounded-full bg-white transition-transform"
+                                :class="active ? 'translate-x-6' : 'translate-x-1'"
+                                />
+                            </Switch>
+                            <Menu as="div" class="relative inline-block text-left">
+                                <MenuButton>
+                                    <EllipsisVertical class="h-6" />
+                                </MenuButton>
+                                <MenuItems class="w-36 bg-white aboslute bottom-0">
+                                    <MenuItem class="flex items-center gap-4">
+                                        <button @click="() => editSkill(s)" class="cursor-pointer">
+                                            <Edit class="w-5 h-5 text-lumy-secondary" aria-hidden="true" />
+                                        </button>
+                                        Edit
+                                    </MenuItem>
+                                    <MenuItem class="flex items-center gap-4">
+                                        <button @click="() => editSkill(s)" class="cursor-pointer">
+                                            <Delete class="w-5 h-5 text-lumy-secondary" aria-hidden="true" />
+                                        </button>
+                                        Delete
+                                    </MenuItem>
+                                </MenuItems>
+                            </Menu>
+                        </div>
+                        <DisclosurePanel class="text-gray-600 text-sm">
+                            {{ s.definition }}
+                        </DisclosurePanel>
                     </div>
-                    <DisclosurePanel class="text-gray-600 text-sm">
-                        {{ s.definition }}
-                    </DisclosurePanel>
                 </div>
             </Disclosure>
         </section>

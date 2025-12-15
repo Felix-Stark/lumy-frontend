360bf3ba14b4040ae5b6882d74b800fc525ac29e
Felix Stark <felix.b.stark@gmail.com>
2025-12-15 13:33:47 +0100

    Update SlackNotAdmin.vue

--- src/views/auth/slack/SlackNotAdmin.vue
+++ src/views/auth/slack/SlackNotAdmin.vue
@@ -4,8 +4,7 @@
 	buttonText="Back to start"
 	@onAction="() => router.push('/')"
 	title="Oh no! This can't be right!"
-	text="It looks like you're not an admin in your Slack workspace, which is required to install this awesomeness.
-Copy the link below and send to one of your admins and you will be set up in no time!"
+	text="It looks like you're not an admin in your Slack workspace, which is required to install this awesomeness. Notify your Slack admin so they can finish the installation!"
 	>
 	</SlackFlowComp>
 </template>
@@ -14,7 +13,6 @@ Copy the link below and send to one of your admins and you will be set up in no 
 import SlackFlowComp from '@/components/slackFlow/SlackFlowComp.vue';
 import lumySad from '@/assets/images/lumy_sad.png';
 import { useRouter } from 'vue-router';
-import { ref } from 'vue';
 const router = useRouter();
 
 </script>

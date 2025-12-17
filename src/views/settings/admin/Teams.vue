df756c78bed028812c19c828f2de4a642a9cbaaf
Felix Stark <felix.b.stark@gmail.com>
2025-12-18 00:58:28 +0100

    Update Teams.vue

--- src/views/settings/admin/Teams.vue
+++ src/views/settings/admin/Teams.vue
@@ -1,10 +1,10 @@
 <template>
     <SettingsTeams v-if="initLoading" />
     <div v-if="adminStore.managers?.length < 1" class="w-full flex justify-center items-center">
-        <div class="flex flex-col items-center w-md gap-4">
+        <div class="flex flex-col items-center w-md gap-2">
             <img :src="LumyConcerned" alt="Lumy is concerned" class="w-38 h-38 mb-4">
-            <p class="text-lg font-semibold">Oh no! No teams?</p>
-            <p class="text-lg">Quick, go to the Users tab and make someone a manager!</p>
+            <p class="text-lg font-semibold">Oh no! No teams...</p>
+            <p class="text-lg">Quick, go to the Users tab <br/> and assign the manager role to the team leaders!</p>
         </div>
     </div>
     <div v-else class="w-full grid grid-cols-3 auto-rows-fr gap-8">

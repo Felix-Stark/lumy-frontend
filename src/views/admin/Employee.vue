30da2d41c972a4c154cbc0dc54faa1f9c3a8ef3f
Felix Stark <felix.b.stark@gmail.com>
2025-12-17 22:55:39 +0100

    Update Employee.vue

--- src/views/admin/Employee.vue
+++ src/views/admin/Employee.vue
@@ -1,7 +1,7 @@
 <template>
 	<DashSkeleton v-if="loading === true" :isOpen="loading" />
-	<div v-else class="flex flex-col items-center gap-6 w-full lg:my-20">
-		<div v-if="employee" class="absolute top-8 left-4 flex items-center mb-8">
+	<div v-else class="flex flex-col items-center gap-6 w-full my-12 lg:my-20">
+		<div v-if="employee" class="absolute top-8 left-4 lg:top-20 flex items-center mb-8">
 			<button @click="resetEmployee()" class="p-2 mr-4 cursor-pointer">
 				<span class="flex items-center gap-2 text-sm text-gray-500 hover:text-gray-700">
 					<ChevronLeft class="size-3" />
@@ -20,7 +20,7 @@
 			</div>
 			<p class="text-lg text-gray-600">{{ formatName(employee.name!) }}</p>
 		</div>
-		<header class="grid grid-cols-2 xl:grid-cols-2 2xl:mx-8 w-full items-stretch gap-6">
+		<header class="grid grid-cols-2 xl:grid-cols-2 2xl:mx-8 lg:mt-8 w-full items-stretch gap-6">
 			<HeadCard
 				:title="`${summary?.feedback_received_count ?? 0}`"
 				description="Feedbacks received"

0d96e6b0f4a4d42237a6c20331cd9e8bd1f2f07c
Felix Stark <felix.b.stark@gmail.com>
2025-12-18 01:44:46 +0100

    Update Integrations.vue

--- src/views/settings/member/Integrations.vue
+++ src/views/settings/member/Integrations.vue
@@ -44,8 +44,8 @@
         <p class="text-center">No access to task descriptions or private comments</p>
         </IntegrationCard>
         <div class="relative self-stretch">
-            <div class="absolute bg-gray-300/80 w-full h-full flex justify-center items-center">
-                <p class="font-bold text-xl">Upcoming feature</p>
+            <div class="absolute bg-gray-300/80 w-full flex justify-center items-center">
+                <p class="font-bold text-xl">Coming soon</p>
             </div>
             <IntegrationCard
             :img="Outlook"

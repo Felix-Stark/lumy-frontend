5019028cd44b2635aba9e7704cd726ff2a4d967c
Felix Stark <felix.b.stark@gmail.com>
2025-12-18 15:50:29 +0100

    subs

---
+++ src/views/settings/admin/Subscriptions.vue
@@ -0,0 +1,20 @@
+<template>
+<article class="flex flex-col items-center bg-white rounded-xl shadow-md p-4 lg:p-8">
+    <h1 class="text-xl font-light text-lumy-purple">Start free. Grow fast. Build a real feedback culture - without the friction.</h1>
+    <section class="grid grid-cols-3 auto-rows-fr">
+        <div class="flex flex-col items-center gap-4 self-stretch p-4 bg-white rounded-xl border border-gray-300">
+            <h2 class="text-lg font-light">Freemium</h2>
+            <h3 class="text-lg px-6 text-lumy-purple">For teams just getting started with feedback</h3>
+            <div class="flex flex-col ">
+                <p><span><Check class="text-lumy-green"/></span> </p>
+            </div>
+        </div>
+    </section>
+</article>
+</template>
+
+<script setup lang="ts">
+import { Check } from 'lucide-vue-next';
+
+
+</script>
